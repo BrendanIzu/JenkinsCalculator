2e25d6d011a8aad134883e10cb90b1940af8189f
brendanizu <izububs4444@gmail.com>
2022-10-21 21:44:00 -0600

    int to binary and uuid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,45 +5,67 @@ public class Main {
     public static void main(String [] args) {
         while(true) {
             System.out.println("Enter in a command");
-            Scanner s = new Scanner(System.in);
 
+            Scanner s = new Scanner(System.in);
             String input = s.nextLine();
-            if (input.contains("quit")) {
-                break;
-            }
+
+            if (input.contains("quit")) break;
 
             Calculator calculator = new Calculator();
-
             String[] inputs = input.split(" ");
+            
             if (Objects.equals(inputs[0], "add")) {
-                int sol = calculator.add(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
-                System.out.println(sol);
+                try {
+                    int sol = calculator.add(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+                    System.out.println(sol);
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
             }
 
             if (Objects.equals(inputs[0], "subtract")) {
-                System.out.println("IN THE ADD FUNCTION");
-                int sol = calculator.subtract(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
-                System.out.println(sol);
+                try {
+                    int sol = calculator.subtract(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+                    System.out.println(sol);
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
             }
 
             if (Objects.equals(inputs[0], "multiply")) {
-                int sol = calculator.multiply(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
-                System.out.println(sol);
+                try {
+                    int sol = calculator.multiply(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+                    System.out.println(sol);
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
             }
 
             if (Objects.equals(inputs[0], "divide")) {
-                int sol = calculator.divide(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
-                System.out.println(sol);
+                try {
+                    int sol = calculator.divide(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+                    System.out.println(sol);
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
             }
 
             if (Objects.equals(inputs[0], "fibonacci")) {
-                int sol = calculator.fibonacciNumberFinder(Integer.parseInt(inputs[1]));
-                System.out.println(sol);
+                try {
+                    int sol = calculator.fibonacciNumberFinder(Integer.parseInt(inputs[1]));
+                    System.out.println(sol);
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
             }
 
             if (Objects.equals(inputs[0], "binary")) {
-                String sol = calculator.intToBinaryNumber(Integer.parseInt(inputs[1]));
-                System.out.println(sol);
+                try {
+                    String sol = calculator.intToBinaryNumber(Integer.parseInt(inputs[1]));
+                    System.out.println(sol);
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
             }
         }
     }
