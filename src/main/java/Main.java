bd48281d9f70e6c91c597f691fd61faae6b11529
brendanizu <izububs4444@gmail.com>
2022-10-21 21:32:34 -0600

    int to binary and uuid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,37 @@ public class Main {
                 break;
             }
 
+            Calculator calculator = new Calculator();
+
             String[] inputs = input.split(" ");
-            for (String value : inputs) {
-                System.out.println(value);
+            if (inputs[0] == "add") {
+                int sol = calculator.add(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+                System.out.println(sol);
+            }
+
+            if (inputs[0] == "subtract") {
+                int sol = calculator.subtract(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+                System.out.println(sol);
+            }
+
+            if (inputs[0] == "multiply") {
+                int sol = calculator.multiply(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+                System.out.println(sol);
+            }
+
+            if (inputs[0] == "divide") {
+                int sol = calculator.divide(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+                System.out.println(sol);
+            }
+
+            if (inputs[0] == "fibonacci") {
+                int sol = calculator.fibonacciNumberFinder(Integer.parseInt(inputs[1]));
+                System.out.println(sol);
+            }
+
+            if (inputs[0] == "binary") {
+                String sol = calculator.intToBinaryNumber(Integer.parseInt(inputs[1]));
+                System.out.println(sol);
             }
         }
     }
